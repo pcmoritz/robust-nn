6b003e04b5d326ff3296487ca1fef8d8a88df329
Philipp Moritz <pcmoritz@gmail.com>
2014-06-01 08:53:33 -0700

    load datafile into current directory

--- logistic_sgd.py
+++ logistic_sgd.py
@@ -157,11 +157,6 @@ def load_data(dataset):
 
     # Download the MNIST dataset if it is not present
     data_dir, data_file = os.path.split(dataset)
-    if data_dir == "" and not os.path.isfile(dataset):
-        # Check if dataset is in the data directory.
-        new_path = os.path.join(os.path.split(__file__)[0], "..", "data", dataset)
-        if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
-            dataset = new_path
 
     if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
         import urllib
